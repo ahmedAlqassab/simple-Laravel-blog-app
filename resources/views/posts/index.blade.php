595fa335f49cd5ad6f33645762a9d79fb688952e
Ahmed alqassab <ahmed-alqassab@live.com>
2019-06-08 14:12:15 +0300

    initial release

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -9,7 +9,7 @@
             <div class="card-body">
                 <div class="row">
                     <div class="col-md-2 col-sm-4">
-                        <a href="/posts/{{$post->id}}"><img style="width:100%" src="/public/cover_images/{{$post->cover_image}}"></a>
+                        <a href="/posts/{{$post->id}}"><img style="width:100%" src="/storage/cover_images/{{$post->cover_image}}"></a>
                     </div>
                     <div class="col-md-8 col-sm-8">
                         <h3><a href="/posts/{{$post->id}}">{{$post->title}}</a></h3>
